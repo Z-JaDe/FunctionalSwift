1360c0dfa0ed7d76f3e2714eff03229367dce432
郑军铎 <zjade@outlook.com>
2018-10-24 19:29:01 +0800

    小修改

--- Sources/Chunk.swift
+++ Sources/Chunk.swift
@@ -9,18 +9,30 @@
 import Foundation
 
 extension Collection {
-    /// ZJaDe: 根据 size 把 Collection 拆分
+    /// ZJaDe: 根据 size 把 Collection 拆分 相当于 chunk(size, 0)
     public func chunk(_ size:Int) -> AnySequence<SubSequence> {
         var temp = self.dropFirst(0)
         return AnySequence { () -> AnyIterator<SubSequence> in
             return AnyIterator({
                 guard temp.count > 0 else { return nil }
+                let size:Int = size > 0 ? size : temp.count
                 defer { temp = temp.dropFirst(size) }
                 return temp.prefix(size)
             })
         }
     }
-    /// ZJaDe: 根据 sizes 把 Collection 拆分, 如果数组里面有 0 对应位置的size等于数组的上一个size
+
+    /** ZJaDe:
+        根据 sizes 把 Collection 拆分,
+        如果数组里面有 0：
+            如果 0 不在数组末尾，对应位置相当于数组上一个有效元素
+            如果 0 在数组末尾，数组后面都以数组最后一个元素有效元素拆分
+            如果找不到上一个有效元素，返回整个剩余集合;
+        例子："11111".chunk([1,2,0]) == ["1","11","11"]
+             "11111".chunk([1,0,2]) == ["1","1","11","1"]
+             "11111".chunk([0]) == ["11111"]
+             "11111".chunk([1]) == ["1","1111"]
+     */
     public func chunk(_ sizes:[Int]) -> AnySequence<SubSequence> {
         var temp = self.dropFirst(0)
         var sizeIterator = sizes.makeIterator()
@@ -28,18 +40,23 @@ extension Collection {
             var preSize:Int?
             return AnyIterator({
                 guard temp.count > 0 else { return nil }
-                let tempSize:Int //= sizeIterator.next().map({$0 > 0 ? $0 : (preSize ?? 0)}) ?? temp.count
-                if let size = sizeIterator.next() {
-                    tempSize = size > 0 ? size : (preSize ?? 0)
-                }else {
-                    tempSize = temp.count
-                }
+                let size:Int = {
+                    if let tempSize = sizeIterator.next() {
+                        return tempSize > 0 ? tempSize : preSize
+                    }else {
+                        return sizes.last.flatMap({$0 > 0 ? nil : preSize})
+                    }
+                }() ?? temp.count
                 defer {
-                    temp = temp.dropFirst(tempSize)
-                    preSize = tempSize
+                    temp = temp.dropFirst(size)
+                    preSize = size
                 }
-                return temp.prefix(tempSize)
+                return temp.prefix(size)
             })
         }
+    }
+    /// ZJaDe: 详见 chunk(_ sizes:[Int])
+    public func chunk(_ sizes:Int...) -> AnySequence<SubSequence> {
+        return chunk(sizes)
     }
 }
